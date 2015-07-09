6f461437574b0e1435ef4cd22751428127f2b6a9
Shusheng Shao <iblackangel@163.com>
2015-07-09 09:06:05 +0800

    update README

--- hello-1.0/src/main.cpp
+++ hello-1.0/src/main.cpp
@@ -10,14 +10,11 @@
 #include <cstring>
 #include <cmath>
 #include <locale>
-#include "config.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	printf("version: %s\n", VERSION);
-	printf("any problems, report to <%s>\n", PACKAGE_BUGREPORT);
     printf("Hello, World!\n");
     return 0;
 }
