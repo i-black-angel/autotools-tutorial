cf5cfe9169d4d90e9c8377931d2152fae7acbe6b
Shusheng Shao <iblackangel@163.com>
2015-07-09 11:00:53 +0800

    add hello-1.0/README

--- hello-1.0/src/main.cpp
+++ hello-1.0/src/main.cpp
@@ -10,11 +10,14 @@
 #include <cstring>
 #include <cmath>
 #include <locale>
+#include "config.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     printf("Hello, World!\n");
+	printf("This is " PACKAGE_STRING "\n");
     return 0;
 }
+
